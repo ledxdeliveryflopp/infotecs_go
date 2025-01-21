8a80539429b31f4732a2ee1991aabd66cc21e40e
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 18:00:46 +0700

    Добавил хэндлеры исключений

--- src/settings/exceptions.go
+++ src/settings/exceptions.go
@@ -23,6 +23,106 @@ func RaiseError(writer http.ResponseWriter, request *http.Request, detail string
 	}
 }
 
+func WalletDontFound(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusNotFound)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("wallet with this number don't found")
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
+func TransactionsDontFound(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusNotFound)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("transactions don't found")
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
+func EncodingError(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusBadRequest)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("error while encoding struct")
+	if err != nil {
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
+func HttpWriteError(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusBadRequest)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("error while send response")
+	if err != nil {
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
+func QueryParamConvertError(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusBadRequest)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("error while converting query param")
+	if err != nil {
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
+func NotEnoughMoneyInWallet(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusBadRequest)
+	var errorSchemas ErrorSchemas
+	json, err := errorSchemas.buildJson("wallet balance is less than requested")
+	if err != nil {
+		log.Println("error while build error json", err)
+		return
+	}
+	_, err = writer.Write(json)
+	if err != nil {
+		log.Println("error while error write json", err)
+		return
+	}
+}
+
 func NotFoundEndpoint(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusNotFound)
