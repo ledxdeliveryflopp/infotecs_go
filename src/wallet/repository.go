c5ef430e9fc18e61a996fe7e4a53272fef694444
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 16:28:42 +0700

    Добавил функцию добавление денег в кошелек

--- src/wallet/repository.go
+++ src/wallet/repository.go
@@ -1,11 +1,12 @@
 package wallet
 
 import (
+	"errors"
 	"fmt"
 	"infotecs_go/src/settings"
 )
 
-func GetWalletByNumber(number string) (Wallet, error) {
+func GetWalletByNumberRepository(number string) (Wallet, error) {
 	db := settings.ConnectToBD()
 	queryStr := fmt.Sprintf("SELECT number, balance FROM wallet WHERE number = '%s'", number)
 	row := db.QueryRow(queryStr)
@@ -15,4 +16,50 @@ func GetWalletByNumber(number string) (Wallet, error) {
 		return walletInfo, err
 	}
 	return walletInfo, err
+}
+
+func UpdateSenderWalletRepository(wallet Wallet, amount float64) error {
+	if wallet.Balance > amount {
+		db := settings.ConnectToBD()
+		newBalance := wallet.Balance - amount
+		queryStr := fmt.Sprintf("UPDATE wallet SET balance = %f WHERE number = '%s'", newBalance, wallet.Number)
+		_, err := db.Exec(queryStr)
+		if err != nil {
+			return err
+		}
+	} else {
+		return errors.New("wallet balance is less than requested")
+	}
+	return nil
+}
+
+func UpdateRecipientWalletRepository(wallet Wallet, amount float64) error {
+	db := settings.ConnectToBD()
+	newBalance := wallet.Balance + amount
+	queryStr := fmt.Sprintf("UPDATE wallet SET balance = %f WHERE number = '%s'", newBalance, wallet.Number)
+	_, err := db.Exec(queryStr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SendMoneyToWalletRepository(from string, to string, amount float64) error {
+	fromWallet, err := GetWalletByNumberRepository(from)
+	if err != nil {
+		return err
+	}
+	toWallet, err := GetWalletByNumberRepository(to)
+	if err != nil {
+		return err
+	}
+	err = UpdateSenderWalletRepository(fromWallet, amount)
+	if err != nil {
+		return err
+	}
+	err = UpdateRecipientWalletRepository(toWallet, amount)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- src/wallet/service.go
+++ src/wallet/service.go
@@ -2,13 +2,15 @@ package wallet
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"infotecs_go/src/settings"
+	"io"
 	"log"
 	"net/http"
 )
 
-func GetWalletInfo(writer http.ResponseWriter, request *http.Request) {
+func GetWalletInfoService(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	walletNumber := mux.Vars(request)["address"]
 	log.Println("address", walletNumber)
@@ -16,7 +18,7 @@ func GetWalletInfo(writer http.ResponseWriter, request *http.Request) {
 		settings.RaiseError(writer, request, "wallet number cannot be less than 0 or equal to 1", 400)
 		return
 	}
-	wallet, err := GetWalletByNumber(walletNumber)
+	wallet, err := GetWalletByNumberRepository(walletNumber)
 	if err != nil {
 		log.Println("error while get wallet info", err)
 		settings.RaiseError(writer, request, "get wallet info error", 400)
@@ -26,6 +28,40 @@ func GetWalletInfo(writer http.ResponseWriter, request *http.Request) {
 	if err != nil {
 		log.Println("error while encoding wallet struct", err)
 		settings.RaiseError(writer, request, "error while encoding wallet struct", 400)
+		return
+	}
+}
+
+func SendMoneyToWalletService(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	var sendSchemas SendMoneySchemas
+	decodedData, err := sendSchemas.decodeJson(request.Body)
+	switch {
+	case err == io.EOF:
+		settings.RaiseError(writer, request, "empty request body", 400)
+		return
+	case err != nil:
+		errorStr := fmt.Sprintf("error while decode: %s", err)
+		settings.RaiseError(writer, request, errorStr, 400)
+		return
+	}
+	err = SendMoneyToWalletRepository(decodedData.From, decodedData.To, decodedData.Amount)
+	if err != nil {
+		log.Println("error while send money to wallet", err)
+		settings.RaiseError(writer, request, "error while send money to wallet", 400)
+		return
+	}
+	var response BaseStruct
+	builtResponse, err := response.buildJson("success")
+	if err != nil {
+		log.Println("error while build response struct", err)
+		settings.RaiseError(writer, request, "error while build response struct", 400)
+		return
+	}
+	_, err = writer.Write(builtResponse)
+	if err != nil {
+		log.Println("error while send response struct", err)
+		settings.RaiseError(writer, request, "error while send response struct", 400)
 		return
 	}
 }
