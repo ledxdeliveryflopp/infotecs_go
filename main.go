850d07c11f3379582e9ab4e8e568c7113dba3bdc
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 18:03:13 +0700

    Добавил embed .env

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"embed"
 	"github.com/gorilla/mux"
 	"infotecs_go/src/settings"
+	"infotecs_go/src/transaction"
 	"infotecs_go/src/wallet"
 	"log"
 	"net/http"
 )
+
+//go:embed environment/*
+var env embed.FS
 
 //go:embed migrations/*.sql
 var dbMigrations embed.FS
@@ -16,12 +20,15 @@ func main() {
 	router := mux.NewRouter()
 	ApiRouter := router.PathPrefix("/api").Subrouter()
 	wallet.SetWalletRouters(ApiRouter)
+	transaction.SetTransactionRouters(ApiRouter)
 	router.NotFoundHandler = http.HandlerFunc(settings.NotFoundEndpoint)
 	router.MethodNotAllowedHandler = http.HandlerFunc(settings.MethodNotAllowed)
+	settings.InitSettings(env)
 	settings.MigrateDatabase(dbMigrations)
 	err := http.ListenAndServe(":1111", router)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	log.Println("application started")
 }
