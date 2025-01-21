2c4ad75a8776ad3b22aae3c079099c664f195ca9
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 18:02:41 +0700

    Удалил константы и строки с url к бд в настройках бд

--- src/settings/database.go
+++ src/settings/database.go
@@ -3,38 +3,23 @@ package settings
 import (
 	"database/sql"
 	"embed"
-	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/rubenv/sql-migrate"
 	"log"
 )
 
-const (
-	host     = "database"
-	port     = 5432
-	user     = "postgres"
-	password = "admin"
-	dbname   = "wallet"
-)
-
 func ConnectToBD() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", GetDatabaseUrl())
 	if err != nil {
 		panic(err)
 	}
-	//defer db.Close()
 	return db
 }
 
 func MigrateDatabase(migrationsPath embed.FS) {
 	migrationsFolder := &migrate.EmbedFileSystemMigrationSource{FileSystem: migrationsPath, Root: "migrations"}
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	log.Println(GetDatabaseUrl())
+	db, err := sql.Open("postgres", GetDatabaseUrl())
 	if err != nil {
 		panic(err)
 	}
