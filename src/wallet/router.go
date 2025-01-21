49c30dc4abe64f7c91b4d138cdeafd03791f3d92
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 16:29:01 +0700

    Добавил эндпоинт добавление денег в кошелек

--- src/wallet/router.go
+++ src/wallet/router.go
@@ -3,5 +3,6 @@ package wallet
 import "github.com/gorilla/mux"
 
 func SetWalletRouters(Router *mux.Router) {
-	Router.HandleFunc("/{address}/balance", GetWalletInfo).Methods("GET")
+	Router.HandleFunc("/{address}/balance", GetWalletInfoService).Methods("GET")
+	Router.HandleFunc("/send", SendMoneyToWalletService).Methods("POST")
 }
