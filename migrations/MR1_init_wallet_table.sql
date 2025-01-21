88659c838c76ece5ad04d59d8511b980d4748bef
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 15:14:33 +0700

    Создал sql запросы
    1) инициализация таблицы кошелька
    2) создание 10 кошельков

---
+++ migrations/MR1_init_wallet_table.sql
@@ -0,0 +1,7 @@
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE wallet (id serial PRIMARY KEY, number varchar, balance float);
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE wallet;
---
+++ migrations/MR2_init_fake_wallets.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+INSERT INTO wallet (number, balance)
+
+SELECT
+    'wlnum' || generate_series(1, 10) AS number,
+     100.0 AS balance;
+
+
+-- +migrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DELETE FROM wallet WHERE id = 1 and id = 2;
