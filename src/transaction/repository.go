c1dd4748ae8856888885a7eece34b8b1e69f3754
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-02-12 17:22:32 +0700

    Добавлен вывод транзакций из кэша

--- src/transaction/repository.go
+++ src/transaction/repository.go
@@ -30,28 +30,34 @@ func CreateTransactionRepository(tx *sql.Tx, sender string, recipient string, am
 //
 // Возвращаемые значения - error при ошибке получения транзакций, []Transaction при удачном получении транзакций
 func GetLastTransactionsRepository(limit int) ([]Transaction, error) {
-	db := settings.ConnectToBD()
-	queryStr := fmt.Sprintf("SELECT sender, recipient, amount, time FROM transaction ORDER BY time DESC LIMIT %d",
-		limit)
-	rows, err := db.Query(queryStr)
+	TransactionsFromRedis, err := getTransactionFromRedis(limit)
 	if err != nil {
-		log.Printf("query messages error: %s", err)
-		return nil, err
-	}
-	defer rows.Close()
-	var transactionSchemas []Transaction
-	for rows.Next() {
-		var t Transaction
-		err := rows.Scan(&t.Sender, &t.Recipient, &t.Amount, &t.Time)
+		db := settings.ConnectToBD()
+		queryStr := fmt.Sprintf("SELECT sender, recipient, amount, time FROM transaction ORDER BY time DESC LIMIT %d",
+			limit)
+		rows, err := db.Query(queryStr)
 		if err != nil {
-			log.Printf("rows scan error: %s", err)
+			log.Printf("query messages error: %s", err)
 			return nil, err
 		}
-		transactionSchemas = append(transactionSchemas, t)
-	}
-	if transactionSchemas != nil {
-		return transactionSchemas, nil
+		defer rows.Close()
+		var transactionSchemas []Transaction
+		for rows.Next() {
+			var t Transaction
+			err := rows.Scan(&t.Sender, &t.Recipient, &t.Amount, &t.Time)
+			if err != nil {
+				log.Printf("rows scan error: %s", err)
+				return nil, err
+			}
+			transactionSchemas = append(transactionSchemas, t)
+		}
+		if transactionSchemas != nil {
+			saveTransactionInRedis(&transactionSchemas, limit)
+			return transactionSchemas, nil
+		} else {
+			return nil, settings.TransactionNotFound
+		}
 	} else {
-		return nil, settings.TransactionNotFound
+		return TransactionsFromRedis, nil
 	}
 }
